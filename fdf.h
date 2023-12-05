58d0568728048f1cb53cbb697c75ed115d27cc53
amajid <abdelilahmajid012@gmail.com>
2023-12-05 20:33:58 +0100

    adding some functionality in parse_file

--- fdf.h
+++ fdf.h
@@ -1,2 +1,15 @@
 #pragma once
 
+#define Z_DIVE 8
+
+typedef struct 
+{
+	float	x;
+	float	y;
+	float	z;
+}	t_point;
+
+typedef struct
+{
+	t_point v[4];
+} t_quad;
---
+++ parse_file.c
@@ -0,0 +1,29 @@
+#include "libft/libft.h"
+#include "ft_printf/ft_printf.h"
+#include "gnl/get_next_line.h"
+#include "fdf.h"
+
+t_quad	*get_split_fdf(int fd)
+{
+	unsigned int	r_size;
+	int				i;
+	int				atoi_ret;
+	
+	r_size = 1000;
+	t_quad *result = malloc(sizeof(t_quad) * r_size);
+	while(1)
+	{
+		char *line = get_next_line(fd);
+		if(!line)
+			break;
+		char **splited_line = ft_split(line, ' ');
+		if(!splited_line)
+			return NULL;
+		i = 0;
+		while(splited_line[i])
+		{
+			ft_atoi(splited_line[i]);
+			i++;
+		}
+	}
+}
